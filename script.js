671c5872240b00baf15c873fc784c2f8d2e1418c
pursakaarkhaan-collab <pursakaarkhaan@gmail.com>
2026-02-26 17:43:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const urIGAS = "https://script.google.com/macros/s/AKfycbxrXPx9zXRgd91OL8Kke2ry6PmIvAc5aAoAauU8fmHO2m5gemC7lU1EDdi0bi55Mx6k/exec";
 
-function panngilFungsi(data) {
+function kirimData(data) {
     fetch(urlGAS, {
-        method: "post",
+        method: "POST",
         mode: "no-cors",
         body:JSON.stringify(data)
     })
